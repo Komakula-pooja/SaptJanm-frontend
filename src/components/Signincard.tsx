a5c39c89536bdaa0ae650fd361128e5a2f6904bb
Komakula-pooja <poojithakomakula21@gmail.com>
2024-12-24 18:50:04 +0530

    first commit

--- src/components/Signincard.tsx
+++ src/components/Signincard.tsx
@@ -22,7 +22,7 @@ const Signincard = () => {
       localStorage.setItem("token", jwt);
       localStorage.setItem("email", postInputs.email);
       alert("Signup successful.");
-      navigate("/dashboard");
+      navigate("/profile");
     } catch (e) {
       alert("Error while signing up.");
     }
