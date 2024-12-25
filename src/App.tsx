45d85f9a6b5a8e133607e07919e660bd383a3cd0
Komakula-pooja <poojithakomakula21@gmail.com>
2024-12-25 23:44:04 +0530

    commit changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import Profile from "./pages/Profile"
 import Dashboard from "./pages/Dashboard"
 import EditProfile from "./pages/EditProfile"
 import OtherProfile from "./pages/OtherProfile"
-import Settings from "./pages/settings"
+
 import { ProtectedRoute } from "./components/protectRoute"
+import FAQs from "./pages/FAQs"
 
 function App() {
   
@@ -22,7 +23,7 @@ function App() {
           <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />}/>} />
           <Route path="/EditProfile" element={<ProtectedRoute element={<EditProfile />}/>} />
           <Route path="/:id" element={<ProtectedRoute element={<OtherProfile />}/>} />
-          <Route path="/settings" element={<ProtectedRoute element={<Settings />}/>} />
+          <Route path="/settings" element={<ProtectedRoute element={<FAQs />}/>} />
       </Routes>
       </BrowserRouter>
     </>
