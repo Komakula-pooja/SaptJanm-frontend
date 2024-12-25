bbc1475cf5ba562ddf1e98f4a17e06bcd2566c75
Komakula-pooja <poojithakomakula21@gmail.com>
2024-12-25 19:19:23 +0530

    commit changes

--- src/components/Designpage.tsx
+++ src/components/Designpage.tsx
@@ -4,7 +4,7 @@ const Designpage = () => {
       return (
         <div className="flex flex-col justify-center items-center text-center h-full bg-gradient-to-r from-red-500 to-pink-500 p-6">
           <div className="mb-4">
-            <GiCrownedHeart className="text-white md:text-9xl sm:w-12 sm:h-12" />
+            <GiCrownedHeart className="text-white md:text-9xl sm:text-6xl" />
           </div>
           <div>
             <h1 className="text-white text-2xl sm:text-xl md:text-2xl lg:text-4xl font-bold mb-2">
