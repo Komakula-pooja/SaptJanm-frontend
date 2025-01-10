9d7823a8b8c623acb3639224a4cde1018e2a20f6
Komakula-pooja <poojithakomakula21@gmail.com>
2025-01-10 10:09:16 +0530

    commit changes

--- src/components/MatchedCard.tsx
+++ src/components/MatchedCard.tsx
@@ -21,11 +21,11 @@ const MatchedCard = ({
     age,
     occupation
 }:MatchedCardProps) => {
-  return (
-    <div className="p-6 bg-red-100 border border-red-200 rounded-lg shadow-md my-4 mx-4 sm:mx-6 lg:mx-8 flex flex-col items-center text-center">
-            <div className="relative w-20 h-20 sm:w-24 sm:h-24 overflow-hidden bg-red-300 rounded-full flex items-center justify-center">
+    return (
+        <div className="p-6 bg-red-100 border border-red-200 rounded-lg shadow-lg my-6 mx-6 sm:mx-8 lg:mx-16 flex flex-col items-center text-center">
+            <div className="relative w-24 h-24 sm:w-28 sm:h-28 lg:w-32 lg:h-32 overflow-hidden bg-red-300 rounded-full flex items-center justify-center">
                 <svg
-                    className="w-16 h-16 sm:w-20 sm:h-20 text-red-600"
+                    className="w-20 h-20 sm:w-24 sm:h-24 lg:w-28 lg:h-28 text-red-600"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -37,23 +37,26 @@ const MatchedCard = ({
                     ></path>
                 </svg>
             </div>
-
-            <div className="mt-2 font-semibold text-gray-800 text-lg sm:text-xl">{name}</div>
-
-            <div className="text-sm text-gray-600 sm:text-base">
+    
+            <div className="mt-4 font-semibold text-gray-800 text-lg sm:text-xl lg:text-2xl">
+                {name}
+            </div>
+    
+            <div className="text-sm text-gray-600 sm:text-base lg:text-lg">
                 {age} years old, {occupation}
             </div>
-
-            <div className="mt-2">
+    
+            <div className="mt-4">
                 <Link
                     to={`/${id}`}
-                    className="text-blue-500 hover:underline"
+                    className="text-blue-500 hover:underline text-sm sm:text-base lg:text-lg"
                 >
                     View More
                 </Link>
             </div>
         </div>
     );
+    
 }; 
 
 export default MatchedCard
